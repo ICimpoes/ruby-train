dede786ce7565b04b22cb0e846dd76705deb5eb8
ilia <ilia@mixbook.com>
2023-12-04 10:17:04 +0200

    Fix digit in the middle of written digit Move to aoc/

--- aoc/2023/1/2.rb
+++ aoc/2023/1/2.rb
@@ -7,7 +7,7 @@ lines = File.read('advent_of_code/2023/1/in.txt')
 # 4nineeightseven2
 # zoneight234
 # 7pqrstsixteen
-# sixrthreeseven74oneightsslnine "
+# sixrthreeseven74oneightssln1ine "
 
 digits = %w[one two three four five six seven eight nine]
 
@@ -20,6 +20,7 @@ lines.each_line do |line|
     if char =~ /[0-9]/
       val = char.to_i
       result[1].append(val)
+      result[0] = []
     else
       result[0].append('')
       res = result[0].each_with_object([[], []]) do |p, res|
@@ -36,8 +37,8 @@ lines.each_line do |line|
     end
   end
 
-  puts "#{line} #{result.first}, #{prev.last}"
-  sum += result[1].first * 10 + prev[1].last
+  # puts "#{line} #{result.first}, #{result.last}"
+  sum += result[1].first * 10 + result[1].last
 end
 
 pp sum
